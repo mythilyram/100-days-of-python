c57e7bb5f91c248720fc908b57b12fab310d0a3d
Mythily Ramanathan <123518126+mythilyram@users.noreply.github.com>
2023-05-26 23:45:26 -0400

    1print.py

--- 1print.py
+++ 1print.py
@@ -2,5 +2,7 @@ print("Welcome to the Band Name Generator")
 city = input("Whats`s the name of the city you grew up in?")
 pet=input("What`s your pet`s name?")
 print("Your band name could be " + city +" "+ pet + "!")
+
+#Tying to print all together
 # #print("Welcome to the Band Name Generator")
 # print("Your band name could be " + input("Whats`s the name of the city you grew up in?") +" "+ input("What`s your pet`s name?") + "!")
