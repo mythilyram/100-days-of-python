88e7b69e8f3d572ba411da9f73a930610edc40e9
Mythily Ramanathan <123518126+mythilyram@users.noreply.github.com>
2023-05-26 23:42:56 -0400

    Add files via upload

---
+++ 1print.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("Whats`s the name of the city you grew up in?")
+pet=input("What`s your pet`s name?")
+print("Your band name could be " + city +" "+ pet + "!")
+# #print("Welcome to the Band Name Generator")
+# print("Your band name could be " + input("Whats`s the name of the city you grew up in?") +" "+ input("What`s your pet`s name?") + "!")
